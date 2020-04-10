7ae0ae6ce6df2f0f25f558656e46fa98b3a51d93
Wagner Ramos <wagner.ap.ramos@gmail.com>
2020-04-10 17:16:30 +0100

    Module 1

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.set("views", path.join(__dirname, "/views"));
+app.set("view engine", 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Index'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('PS Project Running on port 3000!');
+});

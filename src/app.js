f777d3e0bfd26434144b3c9adcb5b7a6539ad4a6
Wagner Ramos <wagner.ap.ramos@gmail.com>
2020-04-10 17:21:40 +0100

    Add static files

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const app = express();
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", 'ejs');
 
+app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/src/public/css')));
 app.use('/js', express.static(path.join(__dirname, '/src/public/js')));
 

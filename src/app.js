8959f31e6211517d45da7e8447815ddfc700ece4
Wagner Ramos <wagner.ap.ramos@gmail.com>
2020-04-13 14:54:37 +0100

    Module 3 done

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ const app = express();
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", 'ejs');
 
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/src/public/css')));
 app.use('/js', express.static(path.join(__dirname, '/src/public/js')));
@@ -49,6 +51,45 @@ app.get('/profile', (req, res) => {
   res.render('profile', {
     user: users[0]
   })
+});
+
+app.get('/transfer', (req, res) => {
+  res.render('transfer');
+});
+app.post('/transfer', (req, res) => {
+  const { from, to, amount } = req.body;
+  const parsedAmount = parseInt(amount);
+
+  let newBalance = accounts[from].balance - parsedAmount;
+  accounts[from].balance = newBalance;
+
+  newBalance = accounts[to].balance + parsedAmount;
+  accounts[to].balance = newBalance;
+
+  const accountsJSON = JSON.stringify(accounts);
+
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'utf-8');
+
+  res.render('transfer', { message: 'Transfer Completed' });
+});
+app.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit });
+});
+app.post('/payment', (req, res) => {
+  const { amount } = req.body;
+  const parsedAmount = parseInt(amount);
+
+  let newBalance = accounts.credit.balance - parsedAmount;
+  accounts.credit.balance = newBalance;
+
+  newBalance = parseInt(accounts.credit.available) + parsedAmount;
+  accounts.credit.available = newBalance;
+
+  const accountsJSON = JSON.stringify(accounts);
+
+  fs.writeFileSync(path.join(__dirname, '/json/accounts.json'), accountsJSON, 'utf-8');
+
+  res.render('payment', { message: "Payment Successful", account: accounts.credit });
 });
 
 app.listen(3000, () => {
